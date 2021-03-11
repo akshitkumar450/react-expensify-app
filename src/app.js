613ace9933d316eb7c51a6c1988013a493ff0363
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-11 23:12:33 +0530

    404 page

--- src/app.js
+++ src/app.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/style.css'
 
-const dashboardPage = () => (
+const DashboardPage = () => (
     <div>
         <p> this is dashboard Page</p>
     </div>
 )
 
-const addExpensePage = () => (
+const AddExpensePage = () => (
     <div>
         <p> this is add expense Page</p>
     </div>
 )
-const editExpensePage = () => (
+const EditExpensePage = () => (
     <div>
         <p> this is edit expense Page</p>
+    </div>
+)
+const Error = () => (
+    <div>
+        <p>404</p>
     </div>
 )
 
 const route = (
     <BrowserRouter>
-        <div>
-            <Route path='/' component={dashboardPage} exact={true}></Route>
-            <Route path='/create' component={addExpensePage}></Route>
-            <Route path='/edit' component={editExpensePage}></Route>
-        </div>
+        <Switch>
+            <Route path='/' component={DashboardPage} exact={true}></Route>
+            <Route path='/create' component={AddExpensePage}></Route>
+            <Route path='/edit' component={EditExpensePage}></Route>
+            <Route component={Error}></Route>
+        </Switch>
     </BrowserRouter>
 )
 
