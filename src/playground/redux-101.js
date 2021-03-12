0bcfe8f03d103589e14474bbc82ec614597ba15e
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-12 21:51:42 +0530

    using subscribe and dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,27 +4,24 @@ import { createStore } from 'redux';
 // first arg is function for state (default state)
 // second arg is action
 const store = createStore((state = { count: 0 }, action) => {
-    // if (action.type === 'INCREMENT') {
-    //     return {
-    //         count: state.count + 1
-    //     }
-    // } else {
-    //     return state;
-    // }
-
-    // switch case is mostly used
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             }
         case 'RESET':
             return {
                 count: 0
+            }
+        case 'SET':
+            return {
+                count: action.count
             }
         default:
             return state
@@ -32,30 +29,48 @@ const store = createStore((state = { count: 0 }, action) => {
 
 })
 
+// this fn get call every time store changes i.e(state changes)
+// basically used ro rerendering
+// it return a function when we want to stop the dispatch
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
 // to get current state object
-console.log(store.getState());
 
 // actions are used to change the redux store(communicating with the store)
 // actions- are the objects that gets sent to the store
 
 // meaning full effect on the store
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    // to give other information(can be access by action)
+    // to use dynamic actions
+    incrementBy: 5
 })
 
 store.dispatch({
     type: 'INCREMENT'
 })
+
+// stop subscribtion
+// unsubscribe()
 
 store.dispatch({
     type: 'RESET'
 })
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 5
 })
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+
 })
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
+// console.log(store.getState());
