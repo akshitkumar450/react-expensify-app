08835624922167a30407959042f42cbe060c63fd
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-13 18:38:40 +0530

    adding action generators for add and remove expense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,12 +1,47 @@
 import { createStore, combineReducers } from 'redux'
+import { v4 as uuidv4 } from 'uuid'; // to get random id
+// action generators
+
+// Add_expense
+const addExpense = ({ des = '', note = '', amount = 0, createdAt = 0 } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuidv4(),
+        des,
+        note,
+        amount,
+        createdAt
+    }
+})
+
+// remove_expense
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    expense: {
+        id
+    }
+})
+// edit_expense
+// sortbydate
+// sortby amount
+// setstartdate
+// setendate
 
 
 // default state for expenses
 const expenseReducerDefaultState = []
 
+// all the  action generators will be dispatched to both  reducers
 // expense reducer
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            // to add a new values to the array
+            return [...state, action.expense]
+
+        case 'REMOVE_EXPENSE':
+            return state.filter((item) => item.id !== action.expense.id)
+
         default:
             return state
     }
@@ -36,7 +71,18 @@ const store = createStore(
     })
 )
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+// we get the action object back when we dispatch which can be used 
+const expenseOne = store.dispatch(addExpense({ des: 'rent', amount: 100 }))
+const expenseTwo = store.dispatch(addExpense({ des: 'car', amount: 1080 }))
+// console.log(expenseOne);
+
+// remove an expense with the given id
+const removeOne = store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+// console.log(removeOne);
 
 const demoState = {
     expenses: [
