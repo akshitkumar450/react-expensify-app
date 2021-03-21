340b6aa468291b74063f79882ed4f5fb2f98ddb4
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-21 21:56:56 +0530

    testing expense form

--- src/tests/__mocks__/moment.js
+++ src/tests/__mocks__/moment.js
@@ -1,5 +1,7 @@
 const moment = jest.requireActual('moment')
 
+// for  real application moment will be the moment library 
+// but for testing it will use mock moment 
 export default (timestamp = 0) => {
     return moment(timestamp)
 }
--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -14,3 +14,52 @@ test('should render expense form with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[0]} />)
     expect(wrapper).toMatchSnapshot()
 })
+
+test('should render error for invalid form submission ', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    expect(wrapper).toMatchSnapshot()
+    // to simulate any handler first we have to find it
+    // second argument is for event handlerer e.preventDeafult()
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    })
+    // to get the state value for testing 
+    expect(wrapper.state('error').length).toBeGreaterThan(0)
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('should set description on input change', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    const value = 'new description'
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('description')).toBe(value)
+})
+
+test('should set note on input change', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    const value = 'new note'
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('note')).toBe(value)
+})
+
+test('should set valid amount on input change', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    const value = '23.50'
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('amount')).toBe(value)
+})
+
+test('should not set invalid amount on input change', () => {
+    const wrapper = shallow(<ExpenseForm />)
+    const value = '23.580'
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    })
+    expect(wrapper.state('amount')).toBe('')
+})
