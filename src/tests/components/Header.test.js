b86182ccf7d83c6c127e495511889d9185f5d88d
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-21 17:31:55 +0530

    testing with enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,26 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'
 import React from 'react'
 import Header from '../../components/Header'
+import { shallow } from 'enzyme'
+
+// if we use  snapshotSerializers in jest.config.json file  then toJSON will be used automatically
+// import toJSON from 'enzyme-to-json'
 
 // shalllow rendering ->it renders only the component
 // full dom rendering-> it renders child compoenent also
 
+// test('should render header correctly', () => {
+//     const renderer = new ReactShallowRenderer()
+//     renderer.render(<Header />)
+//     //  for the first time it will create a new snapshot and it will pass the test case
+//     // after the first test case it will match the output with the existing snapshot
+//     expect(renderer.getRenderOutput()).toMatchSnapshot()
+//     console.log(renderer.getRenderOutput());
+// })
+
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header />)
-    //  for the first time it will create a new snapshot and it will pass the test case
-    // after the first test case it will match the output with the existing snapshot
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
-    console.log(renderer.getRenderOutput());
+    const wrapper = shallow(<Header />)
+    // expect(toJSON(wrapper)).toMatchSnapshot()
+    expect(wrapper).toMatchSnapshot()
+
+    // expect(wrapper.find('h1').text()).toBe('Expensify App')
 })
