271b5494260cc05967aa89dd276aa2a60d6c09cd
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-22 00:20:27 +0530

    testing add expense page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,19 +3,52 @@ import ExpenseForm from './ExpenseForm'
 import { connect } from 'react-redux'
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1> Add expense</h1>
-        <ExpenseForm
-            // when the form get submitted all data will be sent bt ExpenseForm
-            onSubmit={(expense) => {
-                // console.log(expense);
-                // to add new expenses to store
-                props.dispatch(addExpense(expense))
-                // for redirecting to other page (dashboard)
-                props.history.push('/')
-            }}
-        />
-    </div>
-)
-export default connect()(AddExpensePage)
+export class AddExpensePage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(expense) {
+        this.props.onSubmit(expense)
+        this.props.history.push('/')
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Add expense</h1>
+                <ExpenseForm
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
+
+// const AddExpensePage = (props) => (
+//     <div>
+//         <h1> Add expense</h1>
+//         <ExpenseForm
+//             // when the form get submitted all data will be sent bt ExpenseForm
+//             onSubmit={(expense) => {
+//                 // console.log(expense);
+//                 // to add new expenses to store
+//                 // props.dispatch(addExpense(expense))
+//                 // for redirecting to other page (dashboard)
+//                 props.onSubmit(expense)
+//                 props.history.push('/')
+//             }}
+//         />
+//     </div>
+// )
+// for testing 
+const mapDispatchToProps = (dispatch) => {
+    //  used to return props
+    return {
+        onSubmit: (expense) => {
+            return dispatch(addExpense(expense))
+        }
+    }
+}
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AddExpensePage } from '../../components/AddExpensePage'
+import { shallow } from 'enzyme'
+import expenses from '../fixtures/expenses'
+
+test('should render add expense page', () => {
+    const onSubmit = jest.fn()
+    const history = { push: jest.fn() }
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('should handle onSubmit', () => {
+    const onSubmit = jest.fn()
+    const history = { push: jest.fn() }
+    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+    wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
+    expect(history.push).toHaveBeenLastCalledWith('/')
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+})
