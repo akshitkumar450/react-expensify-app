7d0a6d2efc68d708a7c7d55581a780239178d680
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-23 21:48:54 +0530

    setup production for heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
 const publicPath = path.join(__dirname, '..', 'public')
 app.use(express.static(publicPath))
 
@@ -9,6 +8,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log('server started');
 })
