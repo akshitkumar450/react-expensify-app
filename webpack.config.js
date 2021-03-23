eb69d52e0c8fdb0a4125d4ed82e74edd8f3c14f6
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-23 16:47:10 +0530

    webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,45 @@
-// entry point
-// output final bundle file
-
 const path = require('path')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
+module.exports = (env) => {
+    const isProd = env === 'production'
 
-    // loaders
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015']
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+
+        // loaders
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['es2015']
+                    }
+                },
+                //  for loading css
+                {
+                    test: /\.css$/,
+                    use: ["style-loader", "css-loader"],
                 }
-            },
-            //  for loading css
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            }
-        ]
-    },
-    // sorce map --help for debugging-- it will reference to errors in the component file rather than bundle.js file
-    devtool: 'cheap-module-eval-source-map',
+            ]
+        },
+        // sorce map --help for debugging-- it will reference to errors in the component file rather than bundle.js file
+        // we have used different source map in dev and prod 
+        devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
 
-    // a webpack dev server  such as live server
-    // it does not make bundle.js file physically ,,but it loads from memory ,,which make server fast
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+        // a webpack dev server  such as live server
+        // it does not make bundle.js file physically ,,but it loads from memory ,,which make server fast
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 }
+
 
 
