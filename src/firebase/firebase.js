c712bfba78fa34e594ad5f6251eabb83f69d02e4
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-24 22:37:12 +0530

    updating data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,23 +12,38 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 // in ref() is no arg is passed ,it means root of the database
-// firebase.database().ref().set({
-//     name: 'mead',
-//     age: 23,
-//     isSingle: true,
-//     location: {
-//         city: 'philedalphia'
-//     }
-// }).then(() => {
-//     console.log('data is saved');
-// }).catch((error) => {
-//     console.log('error in saving data ', error);
-// })
+firebase.database().ref().set({
+    name: 'mead',
+    age: 23,
+    isSingle: true,
+    location: {
+        city: 'philedalphia'
+    }
+}).then(() => {
+    console.log('data is saved');
+}).catch((error) => {
+    console.log('error in saving data ', error);
+})
 
-firebase.database().ref('isSingle').remove()
-    .then(() => {
-        console.log('data removed');
-    }).catch((error) => {
-        console.log('error in removing data', error);
-    })
+// firebase.database().ref('isSingle').remove()
+//     .then(() => {
+//         console.log('data removed');
+//     }).catch((error) => {
+//         console.log('error in removing data', error);
+//     })
+
+// update can be used to 
+// --update data by passing updated data object
+// -- add a new data 
+// -- delete existing data by setting its value to be null
+// udpate method can't be used to update the nested object values(directly)
+firebase.database().ref().update({
+    name: 'scolfield',
+    age: 49,
+    job: 'SDE',
+    isSingle: null,
+    // for updating the nested object data
+    'location/city': 'boston'
+
+})
 export default firebase
