8985bcf6b78edbf674e95746eef4eb74a9913982
Akshit Kumar <akshitkumar450@gmail.com>
2021-03-25 23:16:09 +0530

    adding logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,7 +1,15 @@
 import firebase, { googleAuthProvider } from '../firebase/firebase'
 
+// login
 export const startLogin = () => {
     return () => {
         firebase.auth().signInWithPopup(googleAuthProvider)
+    }
+}
+
+// logout
+export const startLogout = () => {
+    return () => {
+        firebase.auth().signOut()
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { startLogout } from '../actions/auth'
+import { connect } from 'react-redux'
 
 
 // if we want to show a component on all the pages we put it outside the switch tag
-const Header = () => (
+const Header = (props) => (
     <div>
         <h1>Expensify App</h1>
         <NavLink to='/dashboard' activeClassName='active' exact={true}>Home</NavLink>
         <NavLink to='/create' activeClassName='active'>Create</NavLink>
         <NavLink to='edit' activeClassName='active'>edit</NavLink>
+
+        <button onClick={props.dispatch(startLogout)}>logout</button>
     </div>
 )
-export default Header
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         startLogout: () => dispatch(startLogout())
+//     }
+// }
+
+export default connect()(Header)
